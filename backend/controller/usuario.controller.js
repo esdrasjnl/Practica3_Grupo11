e0877f7e081a47bc1f0d0d2327c24f652612e3d5
Esdras <esdrasjnl@gmail.com>
2020-10-15 21:54:14 -0600

    Se agregaron validaciones para el registro

--- backend/controller/usuario.controller.js
+++ backend/controller/usuario.controller.js
@@ -2,24 +2,48 @@ const usuarioCtrl = {};
 const mysqldb = require('../database');
 
 usuarioCtrl.postUsuario = async function (req, res, next) {
-    const sql = 'insert into usuario set?';
 
-    const UsuarioObj = {
-        user_name: req.body.user_name,
-        correo: req.body.correo,
-        clave: req.body.clave,
-        nombre: req.body.nombre,
-        apellido: req.body.apellido,
-        cui: req.body.cui,
-        edad: req.body.edad,
-        ref_id_tipo: req.body.ref_id_tipo
-    };
+    if (req.body.user_name === undefined || req.body.correo === undefined || req.body.clave === undefined || req.body.nombre === undefined || req.body.apellido === undefined
+        || req.body.cui === undefined || req.body.edad === undefined || req.body.ref_id_tipo === undefined) {
+        return res.json({ 'Msg': 'Faltan Datos' });
+        //console.log(req.body.carnet);
+    } else {
 
-    mysqldb.connection.query(sql, UsuarioObj, error => {
-        if (error) throw error;
-        //res.send('Usuario Creado');
-        res.json({ 'estado': 'true' });
-    });
+        const validacion = `select count(*) from usuario where correo = '${req.body.correo}' and user_name = '${req.body.user_name}' and cui = ${req.body.cui}`;
+
+        var existeDato = 0;
+
+        mysqldb.connection.query(validacion, function (err, results) {
+            if (err) {
+                throw err;
+            }
+            //console.log(results);
+            existeDato = results;
+        });
+
+        if (existeDato === '0') {
+            const sql = 'insert into usuario set?';
+            const UsuarioObj = {
+                user_name: req.body.user_name,
+                correo: req.body.correo,
+                clave: req.body.clave,
+                nombre: req.body.nombre,
+                apellido: req.body.apellido,
+                cui: req.body.cui,
+                edad: req.body.edad,
+                ref_id_tipo: req.body.ref_id_tipo
+            };
+
+            mysqldb.connection.query(sql, UsuarioObj, error => {
+                if (error) throw error;
+                //res.send('Usuario Creado');
+                res.json({ 'estado': 'true' });
+            });
+        } else {
+            res.json({ 'estado': 'Datos Repetidos' });
+        }
+    }
+
 }
 
 module.exports = usuarioCtrl;
