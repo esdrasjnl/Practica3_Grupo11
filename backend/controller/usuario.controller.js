537eab5e7a22a68440d0dc268266135194aeb522
Esdras <esdrasjnl@gmail.com>
2020-10-15 22:35:53 -0600

    Se corrigieron las validaciones del registro

--- backend/controller/usuario.controller.js
+++ backend/controller/usuario.controller.js
@@ -8,39 +8,39 @@ usuarioCtrl.postUsuario = async function (req, res, next) {
         return res.json({ 'Msg': 'Faltan Datos' });
         //console.log(req.body.carnet);
     } else {
-
-        const validacion = `select count(*) from usuario where correo = '${req.body.correo}' and user_name = '${req.body.user_name}' and cui = ${req.body.cui}`;
-
-        var existeDato = 0;
-
-        mysqldb.connection.query(validacion, function (err, results) {
-            if (err) {
-                throw err;
-            }
-            //console.log(results);
-            existeDato = results;
-        });
-
-        if (existeDato === '0') {
-            const sql = 'insert into usuario set?';
-            const UsuarioObj = {
-                user_name: req.body.user_name,
-                correo: req.body.correo,
-                clave: req.body.clave,
-                nombre: req.body.nombre,
-                apellido: req.body.apellido,
-                cui: req.body.cui,
-                edad: req.body.edad,
-                ref_id_tipo: req.body.ref_id_tipo
-            };
-
-            mysqldb.connection.query(sql, UsuarioObj, error => {
-                if (error) throw error;
-                //res.send('Usuario Creado');
-                res.json({ 'estado': 'true' });
+        if (Number.isInteger(req.body.cui) || Number.isInteger(req.body.edad)) {
+            const validacion = `select count(*) as retorno from usuario where correo = '${req.body.correo}' and user_name = '${req.body.user_name}' and cui = ${req.body.cui}`;
+            //var existeDato = 0;
+            mysqldb.connection.query(validacion, function (err, results) {
+                if (err) {
+                    throw err;
+                }
+                //console.log(results[0]);
+                var existeDato = results[0].retorno;
+                console.log(existeDato);
+                if (parseInt(existeDato) === 0) {
+                    const sql = 'insert into usuario set?';
+                    const UsuarioObj = {
+                        user_name: req.body.user_name,
+                        correo: req.body.correo,
+                        clave: req.body.clave,
+                        nombre: req.body.nombre,
+                        apellido: req.body.apellido,
+                        cui: req.body.cui,
+                        edad: req.body.edad,
+                        ref_id_tipo: req.body.ref_id_tipo
+                    };
+                    mysqldb.connection.query(sql, UsuarioObj, error => {
+                        if (error) throw error;
+                        //res.send('Usuario Creado');
+                        res.json({ 'estado': 'true' });
+                    });
+                } else {
+                    res.json({ 'estado': 'Datos Repetidos' });
+                }
             });
         } else {
-            res.json({ 'estado': 'Datos Repetidos' });
+            res.json('Revise los tipos de datos ingresados'); 
         }
     }
 
