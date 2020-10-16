7bf07cfea6eaead6aa4ece24586ae3dfd4a08517
Esdras <esdrasjnl@gmail.com>
2020-10-16 13:02:33 -0600

    Se realizó el metodo para el login

--- backend/routes/usuario.router.js
+++ backend/routes/usuario.router.js
@@ -3,4 +3,6 @@ const router=express.Router();
 const usuario=require('../controller/usuario.controller');
 
 router.post('/agregar',usuario.postUsuario);
+router.post('/login', usuario.loginUsuario);
+
 module.exports=router;
