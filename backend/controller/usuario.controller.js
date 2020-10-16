21268f9c31e5fcef8c8f54cc62f289605ba66996
Esdras <esdrasjnl@gmail.com>
2020-10-15 23:25:21 -0600

    Se agregó la ultima validación

--- backend/controller/usuario.controller.js
+++ backend/controller/usuario.controller.js
@@ -1,49 +1,45 @@
 const usuarioCtrl = {};
 const mysqldb = require('../database');
-
+let vari = 0;
 usuarioCtrl.postUsuario = async function (req, res, next) {
-
-    if (req.body.user_name === undefined || req.body.correo === undefined || req.body.clave === undefined || req.body.nombre === undefined || req.body.apellido === undefined
-        || req.body.cui === undefined || req.body.edad === undefined || req.body.ref_id_tipo === undefined) {
+    let { user_name, correo, clave, nombre, apellido, cui, edad, ref_id_tipo } = req.body;
+    console.log(Number.isInteger(vari));
+    let parametrosValidos = !user_name || !correo || !nombre || !apellido || isNaN(cui) || isNaN(cui) || !ref_id_tipo;
+    if (parametrosValidos) {
         return res.json({ 'Msg': 'Faltan Datos' });
         //console.log(req.body.carnet);
     } else {
-        if (Number.isInteger(req.body.cui) || Number.isInteger(req.body.edad)) {
-            const validacion = `select count(*) as retorno from usuario where correo = '${req.body.correo}' and user_name = '${req.body.user_name}' and cui = ${req.body.cui}`;
-            //var existeDato = 0;
-            mysqldb.connection.query(validacion, function (err, results) {
-                if (err) {
-                    throw err;
-                }
-                //console.log(results[0]);
-                var existeDato = results[0].retorno;
-                console.log(existeDato);
-                if (parseInt(existeDato) === 0) {
-                    const sql = 'insert into usuario set?';
-                    const UsuarioObj = {
-                        user_name: req.body.user_name,
-                        correo: req.body.correo,
-                        clave: req.body.clave,
-                        nombre: req.body.nombre,
-                        apellido: req.body.apellido,
-                        cui: req.body.cui,
-                        edad: req.body.edad,
-                        ref_id_tipo: req.body.ref_id_tipo
-                    };
-                    mysqldb.connection.query(sql, UsuarioObj, error => {
-                        if (error) throw error;
-                        //res.send('Usuario Creado');
-                        res.json({ 'estado': 'true' });
-                    });
-                } else {
-                    res.json({ 'estado': 'Datos Repetidos' });
-                }
-            });
-        } else {
-            res.json('Revise los tipos de datos ingresados'); 
-        }
+        const validacion = `select count(*) as retorno from usuario where correo = '${req.body.correo}' and user_name = '${req.body.user_name}' and cui = ${cui}`;
+        //var existeDato = 0;
+        mysqldb.connection.query(validacion, function (err, results) {
+            if (err) {
+                throw err;
+            }
+            //console.log(results[0]);
+            var existeDato = results[0].retorno;
+            console.log(existeDato);
+            if (parseInt(existeDato) === 0) {
+                const sql = 'insert into usuario set?';
+                const UsuarioObj = {
+                    user_name: req.body.user_name,
+                    correo: req.body.correo,
+                    clave: req.body.clave,
+                    nombre: req.body.nombre,
+                    apellido: req.body.apellido,
+                    cui: req.body.cui,
+                    edad: req.body.edad,
+                    ref_id_tipo: req.body.ref_id_tipo
+                };
+                mysqldb.connection.query(sql, UsuarioObj, error => {
+                    if (error) throw error;
+                    //res.send('Usuario Creado');
+                    res.json({ 'estado': 'true' });
+                });
+            } else {
+                res.json({ 'estado': 'Datos Repetidos' });
+            }
+        });
     }
-
 }
 
 module.exports = usuarioCtrl;
