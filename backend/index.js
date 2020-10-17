7880b24cd48d13d7c1dd0ceb497aeecae9ef2078
yovany <syovany@gmail.com>
2020-10-17 17:16:08 -0600

    Se agrego dependencia Cors en backend

--- backend/index.js
+++ backend/index.js
@@ -2,11 +2,13 @@ const bodyparser = require('body-parser');
 var express = require('express')
 var http = require('http');
 var app = express();
+var cors=require('cors');
 var mysqldb =require('./database');
 const PORT = process.env.PORT || 4000;
 
 app.use(bodyparser.json());
 app.use(bodyparser.urlencoded({ extended: true }));
+app.use(cors());
 
 app.listen(PORT, ()=>{
     console.log(`Servidor Corriendo en el puerto ${PORT}`);
