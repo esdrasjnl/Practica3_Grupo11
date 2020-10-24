acccc0890c29e45ff6cb102fc387f323f3b32e07
AstridHG <astrid1404hg@gmail.com>
2020-10-24 01:28:04 -0600

    Se realizo el carrito de compras

--- frontend-P3/src/app/components/carrito/carrito.component.ts
+++ frontend-P3/src/app/components/carrito/carrito.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute, Params } from '@angular/router';
+import { CompraService } from "../../services/compra.service"
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-carrito',
@@ -8,7 +9,7 @@ import { ActivatedRoute, Params } from '@angular/router';
 })
 export class CarritoComponent implements OnInit {
 
-  constructor(private rutaActiva: ActivatedRoute) { }
+  constructor(public service: CompraService, public router: Router) { }
   p:any=[];
   user_string : string =localStorage.getItem('Compras');
   Datos: any =JSON.parse(this.user_string);
@@ -17,7 +18,31 @@ export class CarritoComponent implements OnInit {
    //this.rutaActiva.snapshot.params
     //console.log(this.rutaActiva.snapshot.params.card);
     //console.log(this.rutaActiva.snapshot.params.precio);
-    console.log(this.Datos);
+    this.obtenerTasa();
+    this.PagoTotal();
   }
+
+  tasa:any;
+  obtenerTasa() {
+    this.service.getTasaCambio()
+      .subscribe(
+        res => {
+          this.tasa = res[0].total;
+          return this.tasa;
+        },
+        err => console.log(err)
+      )
+  }
+  
+  total = 0;
+  total1 = 0;
+  PagoTotal()
+  {
+    for (let i = 0; i < this.Datos.length; i++) {
+      this.total += this.Datos[i].precio * this.Datos[i].repite
+    }
+
+  }
+
 
 }
--- frontend-P3/src/app/services/compra.service.ts
+++ frontend-P3/src/app/services/compra.service.ts
@@ -19,6 +19,10 @@ export class CompraService {
     return this.http.get<any>(`${this.api}/Value`);
   }
 
+  getTasaCambio() {
+    return this.http.get<any>(`${this.api}/TasaCambio`);
+  }
+
   setdatos(user: any) {
     let user_string = JSON.stringify(user);
     localStorage.setItem('Compras', user_string);
