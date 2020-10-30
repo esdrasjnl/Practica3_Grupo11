5605032898f42baf6fe0c171010517c8e6309c87
Esdras <esdrasjnl@gmail.com>
2020-10-30 17:33:46 -0600

    Se agregaron cambios a la BD

--- practica3_script.sql
+++ practica3_script.sql
@@ -19,28 +19,6 @@ CREATE TABLE usuario(
 	CONSTRAINT fk_tipo  FOREIGN KEY(ref_id_tipo) REFERENCES tipo_usuario(id_tipo) ON DELETE CASCADE 
 );
 
-CREATE TABLE tarjeta_credito(
-	id_tarjeta			INTEGER PRIMARY KEY,
-	num_tarjeta			VARCHAR(16) NOT NULL,
-    nombre				VARCHAR(50) NOT NULL,
-    fecha				VARCHAR(10) NOT NULL,
-    codigo				INTEGER NOT NULL,
-    monto_pago			INTEGER NOT NULL,
-    moneda				VARCHAR(10) NOT NULL,
-    ref_id_usuario		INTEGER NOT NULL,
-    
-    CONSTRAINT fk_usuario  FOREIGN KEY(ref_id_usuario) REFERENCES usuario(id_usuario) ON DELETE CASCADE 
-);
-
-CREATE TABLE historial(
-	id_historia			INTEGER PRIMARY KEY AUTO_INCREMENT,
-    nombre				VARCHAR(20),
-    imagen				VARCHAR(100),
-    estado				INTEGER,
-    ref_id_usuario		INTEGER NOT NULL,
-    CONSTRAINT fk_usuario_historial  FOREIGN KEY(ref_id_usuario) REFERENCES usuario(id_usuario) ON DELETE CASCADE 
-);
-
 create table compras(
 	idCompra integer primary key auto_increment,
 	pkUser integer,    
@@ -55,20 +33,18 @@ create table compras(
 
 create table giffCard(
 	idGCard integer primary key auto_increment,
-	cantidad integer,
 	nombre varchar(100),
 	image varchar(250),
 	precio double,
 	recargo double,
-	total double
+    estado varchar(10)
 );
-
-alter table giffCard add estado varchar(10);
 
 create table detalleCompra(
 	idDetCom integer primary key auto_increment,
+    cantidad integer,
     subtotal double,
-    pkgCard integer,
+    pkgCard integer, 
     pkComp integer,
     constraint pkeyGCard foreign key(pkgCard) references giffCard(idGCard) on delete cascade,
     constraint pketComp foreign key(pkComp) references compras(idCompra) on delete cascade
