596d09ba9882ba59d809107238b16bb2a6cad03b
wendychamale <wendy.chamaleb@gmail.com>
2020-10-31 15:14:37 -0600

    se agrego servicio y conexion con backend, historia regalo

---
+++ frontend-P3/src/app/services/usuario.service.ts
@@ -0,0 +1,19 @@
+import { Injectable } from '@angular/core';
+import {HttpClient,HttpHeaders}from "@angular/common/http"
+import { map, throttleTime } from "rxjs/operators";
+
+@Injectable({
+  providedIn: 'root'
+})
+export class UsuarioService {
+
+  constructor(private http: HttpClient) { }
+
+  
+  api = 'http://localhost:4000/api';
+
+  getusuario()
+    {
+      return this.http.get<any>(`${this.api}/usuario/usuario`);
+    }
+}
