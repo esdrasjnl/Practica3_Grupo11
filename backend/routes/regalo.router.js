9a2f8f6e62d922450fc71f834e62a7f2a87e56ef
yovany <syovany@gmail.com>
2020-10-31 16:48:19 -0600

    Se actualizo parametro en router para igual  a lo que se esperaba en la funcion

--- backend/routes/regalo.router.js
+++ backend/routes/regalo.router.js
@@ -5,5 +5,5 @@ const router =express.Router();
 
 router.post('/agregar',regalo.postRegalo);
 router.post('/agregar/detalle',regalo.postDetalleRegalo);
-router.get('/:carnet',regalo.getListBuyForUser);
+router.get('/:iduser',regalo.getListBuyForUser);
 module.exports=router;
