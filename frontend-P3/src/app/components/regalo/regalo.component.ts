6ec94d3edd240254a3e7d96aebf432b64186cfd8
wendychamale <wendy.chamaleb@gmail.com>
2020-10-31 15:06:40 -0600

    se agrego componente para historia de regalo

---
+++ frontend-P3/src/app/components/regalo/regalo.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-regalo',
+  templateUrl: './regalo.component.html',
+  styleUrls: ['./regalo.component.css']
+})
+export class RegaloComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
