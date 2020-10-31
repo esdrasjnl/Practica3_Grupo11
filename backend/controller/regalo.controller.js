5a1ae20606f8e83d4e80a55717820ef71b79e231
yovany <syovany@gmail.com>
2020-10-31 17:04:01 -0600

    actualizaciones basicas

--- backend/controller/regalo.controller.js
+++ backend/controller/regalo.controller.js
@@ -51,7 +51,6 @@ regaloCtrl.postDetalleRegalo=async function(req,res){
     }
 }
 regaloCtrl.getListBuyForUser= async function(req,res){
-    console.log("pasando---------");
     let {iduser}=req.params; //carnet en este caso es id
     let validaParametro=isNaN(iduser) ||iduser==' ';
     if(validaParametro){
